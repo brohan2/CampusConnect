9299402cc87b65fbe298d1c492a8830581875370
brohan2 <21R11A6608@gcet.edu.in>
2024-05-27 18:22:01 +0530

    added home page and connected  but not designed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,14 @@
-import 'package:campusconnect/utilities/add_project.dart';
+import 'package:campusconnect/screens/connect.dart';
+import 'package:campusconnect/screens/teamup.dart';
+import 'package:campusconnect/screens/threads.dart';
 import 'package:campusconnect/utilities/topnavbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
-import 'package:campusconnect/utilities/search_bar.dart';
+// import 'package:campusconnect/utilities/search_bar.dart';
 import 'package:campusconnect/utilities/sidebar_drawer.dart';
-import 'package:campusconnect/utilities/feild_selection.dart';
-import 'package:campusconnect/utilities/project_listing.dart';
+// import 'package:campusconnect/utilities/feild_selection.dart';
+// import 'package:campusconnect/utilities/project_listing.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 class Home extends StatelessWidget {
@@ -25,58 +27,80 @@ class Home extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(height: 20,),
-          LandingPage(),
-          LandingPage(),
-        ],
-      )
+          LandingPage(title: "Teamup",color:"blue"),
+          LandingPage(title : "Connections",color:"red"),
+          LandingPage(title : "Thread",color:"red"),
 
+        ],
+      ),
     );
   }
 }
 class LandingPage extends StatelessWidget {
+  final String title;
+  final String color;
+  LandingPage({required this.title , required this.color});
+
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                // Handle container 1 click
-                print('Container 1 clicked!');
-              },
-              child: Container(
-                width: 300,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: Text(
-                    'Container 1',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              // Handle container click
+              print('$title clicked!');
+              switch(title){
+                case "Teamup":
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Teamup()),
+                );   
+                case "Connections":
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Connect()),
+                );   
+                case "Thread":
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThreadsPage()),
+                );
+               
+              }
+                   },
+            child: Container(
+              width: 300,
+              height: 200,
+              decoration: BoxDecoration(
+
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            
-          ],
-        ),
-      );
-  
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/teamup.dart
+++ lib/screens/teamup.dart
@@ -1,4 +1,3 @@
-import 'package:campusconnect/utilities/project_listing.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
@@ -41,25 +40,43 @@ class Teamup extends StatelessWidget {
       drawer: SidebarDrawer(drawerHeaderHeight: drawerHeaderHeight),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Welcome to Campus Connect',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
+            SizedBox(height: 10.0),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Connect with projects and internships that match your skills and interests.',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'You can add a project here',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Navigate to AddProjectPage when button is pressed
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddProjectPage()),
+                      );
+                    },
+                    style: TextButton.styleFrom(
+                      elevation: 15,
+                      backgroundColor: Color.fromARGB(0, 254, 237, 237),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      textStyle: TextStyle(fontSize: 18),
+                    ),
+                    child: Text(
+                      'Add Project',
+                      style: TextStyle(color: Color.fromARGB(255, 97, 78, 206)),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
             SearchBar1(),
             FilterButtonList(),
             Padding(
@@ -120,45 +137,86 @@ class Teamup extends StatelessWidget {
               ],
             ),
             SizedBox(height: 20),
-            FloatingActionButton(
-              onPressed: () {
-                // Navigate to AddProjectPage when FAB is pressed
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddProjectPage()),
-                );
-              },
-              child: Icon(Icons.add),
-              backgroundColor: Colors.orange,
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'What our users say',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                '"Campus Connect has been a game-changer for my career!" - Student A',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-              child: Text(
-                '"I found the perfect internship through Campus Connect." - Student B',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 20),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ProjectListing extends StatelessWidget {
+  final ImageProvider image;
+  final String title;
+  final String description;
+  final List<String> skillsRequired;
+  final VoidCallback onPressedExplore;
+
+  const ProjectListing({
+    required this.image,
+    required this.title,
+    required this.description,
+    required this.skillsRequired,
+    required this.onPressedExplore,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image(
+                image: image,
+                height: 150,
+                width: double.infinity,
+                fit: BoxFit.cover),
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            description,
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Skills Required:',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Wrap(
+            spacing: 5,
+            children: skillsRequired
+                .map((skill) => Chip(label: Text(skill)))
+                .toList(),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: ElevatedButton(
+              onPressed: onPressedExplore,
+              child: Text('Explore'),
+            ),
+          ),
+        ],
       ),
     );
   }
